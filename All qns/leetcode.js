03d9a645d05472cb38e843e6be991f233b9e8b31
Muhammed Sahad P <zanusahad@gmail.com>
2025-03-22 11:55:35 +0530

    solve finding diff of two arrays pgm

--- All qns/leetcode.js
+++ All qns/leetcode.js
@@ -1116,3 +1116,14 @@
 // };
 // console.log(smallestEvenMultiple(5)); //10
 // console.log(smallestEvenMultiple(6)); //6
+
+//! 2215. Find the Difference of Two Arrays
+var findDifference = function (nums1, nums2) {
+  let set1 = new Set(nums1);
+  let set2 = new Set(nums2);
+  let res1 = [...set1].filter((num) => !set2.has(num));
+  let res2 = [...set2].filter((num) => !set1.has(num));
+  return [res1, res2];
+};
+console.log(findDifference([1, 2, 3], [2, 4, 6]));
+console.log(findDifference([1, 2, 3, 3], [1, 1, 2, 2]));
